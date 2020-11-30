1d65ae260de7200fb855a3b94198fc9c6437b892
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-11-30 07:15:44 +0100

    generate shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
