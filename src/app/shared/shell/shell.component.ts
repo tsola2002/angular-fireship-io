982d9de380d0e2c4f06f78f3d2cfbf13edace6a2
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-12-01 11:01:57 +0100

    app navigation built out

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnInit {
 
-  constructor() { }
+export class ShellComponent {
 
-  ngOnInit(): void {
-  }
+  // we inject the breakpoint observer into the constructor
+  constructor(private breakpointObserver: BreakpointObserver ) {}
+
+  // we have acess to an observable dat listens to different breakpoints
+  // & helps us to react to different viewport sizes
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+  // we pipe in some operators here
+  // we map the result of the breakpoint change, so we map to a single value 
+  // shareReplay will allow us subscribe to an observable multiple times in a template using a async pipe
+  .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
 }
