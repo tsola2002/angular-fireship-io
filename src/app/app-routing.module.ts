0bf4e695631c794e9074be346849e8e38f99bc87
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2021-01-05 16:48:23 +0100

    added lazy-loaded login feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  // the module is dynamically imported code in the module
+  // angular doesnt bundle the code in this module
+  {
+    path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  }
 ];
 
 @NgModule({
